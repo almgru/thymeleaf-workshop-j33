6de56c0398528c6e8d384b211d44d77abb17fe08
Daniel Alm Grundström <daniel.alm.grundstrom@pm.me>
2021-03-22 13:12:06 +0100

    Project setup

---
+++ src/main/java/se/lexicon/almgru/thymeleafworkshopj33/ThymeleafWorkshopJ33Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.almgru.thymeleafworkshopj33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafWorkshopJ33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafWorkshopJ33Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/almgru/thymeleafworkshopj33/ThymeleafWorkshopJ33ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.almgru.thymeleafworkshopj33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafWorkshopJ33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
