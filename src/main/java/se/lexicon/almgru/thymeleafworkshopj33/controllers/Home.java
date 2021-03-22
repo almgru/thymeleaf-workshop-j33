d51a06df90191571dd6038210fc47b582bf718ae
Daniel Alm Grundström <daniel.alm.grundstrom@pm.me>
2021-03-22 13:30:08 +0100

    Adds /contact and /contact-list pages

--- src/main/java/se/lexicon/almgru/thymeleafworkshopj33/controllers/Home.java
+++ src/main/java/se/lexicon/almgru/thymeleafworkshopj33/controllers/Home.java
@@ -1,12 +1,40 @@
 package se.lexicon.almgru.thymeleafworkshopj33.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Home {
+    private final List<String> contacts = new ArrayList<>();
+
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/contact-list")
+    public String contactList(Model model) {
+        model.addAttribute("contacts", contacts);
+        return "contact-list";
+    }
+
+    @PostMapping("/contact")
+    public String contact(@RequestParam("name") String name) {
+        if (name != null && !name.isEmpty()) {
+            contacts.add(name);
+        }
+
+        return "redirect:/contact-list";
     }
 }
